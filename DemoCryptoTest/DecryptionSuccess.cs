79c6ad1404a3cb7a1df1d629e774910136713151
Wendell White <avengerine@gmail.com>
2015-10-24 17:02:06 +0100

    Change name for test clases

--- DemoCryptoTest/DecryptionSuccess.cs
+++ DemoCryptoTest/DecryptionSuccess.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DemoCrypto;
+using Should;
+using NUnit.Framework;
+using SpecsFor;
 
 namespace DemoCryptoTest
 {
-    class DecryptionSuccess
+    class DecryptionSuccess : SpecsFor<DemoCryptoClass>
     {
+        private string password = "password";
+        private string plainText = "This is the plain text..";
+        private string cipherText = "";
+        private string recoveredText = "";
+
+        protected override void Given()
+        {
+            cipherText = SUT.EncryptText(password, plainText);
+        }
+
+        protected override void When()
+        {
+            recoveredText = SUT.DecryptText(password, cipherText);
+        }
+
+        [Test]
+        public void TextRecoveredMatchesPlainText()
+        {
+            recoveredText.ShouldEqual(plainText);
+        }
     }
 }
